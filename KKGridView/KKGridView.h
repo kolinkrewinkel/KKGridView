d87d0693e0dd866e05d32ead4cb3c267dc2301e0
Kolin Krewinkel <kolin.krewinkel@me.com>
2011-07-24 18:33:17 -0700

    Added delegate+datasource protocols along with basic properties & initialization methods.

--- KKGridView/KKGridView.h
+++ KKGridView/KKGridView.h
@@ -6,11 +6,46 @@
 //  Copyright 2011 contributors. All rights reserved.
 //
 
+#import "KKGridViewCell.h"
 
+@protocol KKGridViewDataSource, KKGridViewDelegate;
 
 @interface KKGridView : UIScrollView {
     @private
-    
+    id <KKGridViewDataSource> _dataSource;
+    id <KKGridViewDelegate> _gridDelegate;
 }
+
+@property (nonatomic, assign) id <KKGridViewDataSource> dataSource;
+@property (nonatomic, assign) id <KKGridViewDelegate> gridDelegate;
+
+#pragma mark - Properties
+
+#pragma mark - Initializers
+
+- (id)initWithFrame:(CGRect)frame dataSource:(id <KKGridViewDataSource>)dataSource delegate:(id <KKGridViewDelegate>)delegate;
+
+@end
+
+#pragma mark - KKGridViewDataSource
+
+@protocol KKGridViewDataSource <NSObject>
+
+@required
+
+- (NSUInteger)gridView:(KKGridView *)gridView numberOfRowsInSection:(NSUInteger)section;
+- (KKGridViewCell *)gridView:(UITableView *)gridView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+
+@optional
+
+- (NSUInteger)numberOfSectionsInGridView:(KKGridView *)gridView;
+
+@end
+
+#pragma mark - KKGridViewDelegate
+
+@protocol KKGridViewDelegate <NSObject>
+
+- (void)gridView:(KKGridView *)gridView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
 
 @end
--- KKGridView/KKGridView.m
+++ KKGridView/KKGridView.m
@@ -8,13 +8,56 @@
 
 #import "KKGridView.h"
 
+@interface KKGridView ()
+
+- (void)_sharedInitialization;
+
+@end
+
 @implementation KKGridView
+
+@synthesize dataSource = _dataSource;
+@synthesize gridDelegate = _gridDelegate;
+
+#pragma mark - Initialization Methods
+
+- (void)_sharedInitialization
+{
+    
+}
 
 - (id)init
 {
-    self = [super init];
-    if (self) {
-        // Initialization code here.
+    if ((self = [super init])) {
+        [self _sharedInitialization];
+    }
+    
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if ((self = [super initWithCoder:aDecoder])) {
+        [self _sharedInitialization];
+    }
+    
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    if ((self = [super initWithFrame:frame])) {
+        [self _sharedInitialization];
+    }
+    
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame dataSource:(id<KKGridViewDataSource>)dataSource delegate:(id<KKGridViewDelegate>)delegate
+{
+    if ((self = [self initWithFrame:frame])) {
+        _dataSource = dataSource;
+        _gridDelegate = delegate;
     }
     
     return self;
