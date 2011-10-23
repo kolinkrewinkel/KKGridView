kolinkrewinkel/KKGridView

commit d3cb6c24be70d6a1da3805bf658efc09eeff494f

Syncrhonized header & footer heights, fixed bug in which the last sticky footers would be atop the scroll indicator.

KKGridView/KKGridView.mm
@@ -15,7 +15,6 @@
 #import <map>
 #import <vector>
 
-#define KKGridViewDefaultAnimationDuration 0.25f
 #define KKGridViewDefaultAnimationStaggerInterval 0.025
 
 @interface KKGridView () {
@@ -315,7 +314,8 @@
                     f.origin.y = sectionTwoY + sectionTwoHeight;
                 }
             }
-            [self bringSubviewToFront:footer.view];
+            [footer.view removeFromSuperview];
+            [self insertSubview:footer.view aboveSubview:[[_visibleCells allValues] objectAtIndex:0]];
         } else {
             // footer isn't sticky anymore, set originTop to saved position
             f.origin.y = footer->stickPoint;

Examples/GridViewDemo/GridViewDemo/GridViewDemoViewController.m
@@ -147,7 +147,7 @@ static const NSUInteger kNumSection = 40;
 
 - (CGFloat)gridView:(KKGridView *)gridView heightForFooterInSection:(NSUInteger)section
 {
-    return 35.f;
+    return 25.f;
 }
 
 - (UIView *)gridView:(KKGridView *)gridView viewForFooterInSection:(NSUInteger)section

KKGridView/Definitions.h
@@ -8,10 +8,9 @@
 
 #import <AvailabilityInternal.h>
 
-// Constants
-
-
 // Defines
+
+#define KKGridViewDefaultAnimationDuration 0.25
 
 // Macros
 

KKGridView/KKGridViewCell.h
@@ -6,6 +6,9 @@
 //  Copyright 2011 Giulio Petek, Jonathan Sterling, and Kolin Krewinkel. All rights reserved.
 //
 
+// For quick reference:
+// UITableViewCell
+
 @class KKGridView;
 @class KKIndexPath;
 
@@ -28,8 +31,10 @@
 @property (nonatomic, copy) NSString *reuseIdentifier;
 @property (nonatomic) BOOL selected;
 @property (nonatomic, strong) UIView *selectedBackgroundView;
+@property (nonatomic) BOOL editing;
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated;
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated;
 
 #pragma mark - Subclassers
 

KKGridView/KKGridViewCell.m
@@ -13,6 +13,7 @@
 
 @synthesize backgroundView = _backgroundView;
 @synthesize contentView = _contentView;
+@synthesize editing = _editing;
 @synthesize selectedBackgroundView = _selectedBackgroundView;
 @synthesize reuseIdentifier = _reuseIdentifier;
 @synthesize selected = _selected;
@@ -93,10 +94,11 @@
     }];
 }
 
-- (void)setIndexPath:(KKIndexPath *)indexPath
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated
 {
-    _indexPath = [indexPath copy];
-    [self setNeedsDisplay];
+    [UIView animateWithDuration:KKGridViewDefaultAnimationDuration animations:^{
+        self.editing = editing;
+    }];
 }
 
 #pragma mark - Layout

commit 6eece41612d50ae614bb5c750ba745a42a72e22b

Fixed view ordering issue introduced in d3cb6c24be70d6a1da3805bf658efc09eeff494f
Now, the footer view goes on top of the cells, and the scroller goes on top of
the footer view. I'm open to better solutions than this subview index stuff,
though.

KKGridView/KKGridView.mm
@@ -312,8 +312,10 @@
                     f.origin.y = sectionTwoY + sectionTwoHeight;
                 }
             }
+            
+            // move footer view to right below scroller
             [footer.view removeFromSuperview];
-            [self insertSubview:footer.view aboveSubview:[[_visibleCells allValues] objectAtIndex:0]];
+            [self insertSubview:footer.view atIndex:self.subviews.count - 1];
         } else {
             // footer isn't sticky anymore, set originTop to saved position
             f.origin.y = footer->stickPoint;