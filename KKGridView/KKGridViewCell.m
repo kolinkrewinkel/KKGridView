20a69fb47e055c05cd189755a7304c38dcce069e
Martin Alleus <martin@appcorn.se>
2012-01-12 12:27:46 +0100

    Moved from calling [self layoutSubviews] to [self setNeedsLayout] for a pretty major performance boost

--- KKGridView/KKGridViewCell.m
+++ KKGridView/KKGridViewCell.m
@@ -99,8 +99,10 @@
 
 - (void)setAccessoryType:(KKGridViewCellAccessoryType)accessoryType
 {
-    _accessoryType = accessoryType;
-    [self setNeedsLayout];
+	if (_accessoryType != accessoryType) {
+		_accessoryType = accessoryType;
+		[self setNeedsLayout];
+	}
 }
 
 - (void)setEditing:(BOOL)editing animated:(BOOL)animated
@@ -112,25 +114,31 @@
 
 - (void)setSelected:(BOOL)selected
 {
-    _selected = selected;
-    [self layoutSubviews];        
+	if (selected != _selected) {
+		_selected = selected;
+		[self setNeedsLayout];
+	}
 }
 
 - (void)setHighlighted:(BOOL)highlighted
 {
-    _highlighted = highlighted;
-    [self layoutSubviews];
+	if (_highlighted != highlighted) {
+		_highlighted = highlighted;
+		[self setNeedsLayout];
+	}
 }
 
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
-    [UIView animateWithDuration:animated ? 0.2 : 0 delay:0 options:(UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionAllowAnimatedContent) animations:^(void) {
-        _selected = selected;
-        _selectedBackgroundView.alpha = selected ? 1.f : 0.f;
-    } completion:^(BOOL finished) {
-        [self layoutSubviews];        
-    }];
+	if (selected != selected) {
+		[UIView animateWithDuration:animated ? 0.2 : 0 delay:0 options:(UIViewAnimationOptionAllowUserInteraction | UIViewAnimationOptionAllowAnimatedContent) animations:^(void) {
+			_selected = selected;
+			_selectedBackgroundView.alpha = selected ? 1.f : 0.f;
+		} completion:^(BOOL finished) {
+			[self setNeedsLayout];        
+		}];
+	}
 }
 
 - (void)_updateSubviewSelectionState
